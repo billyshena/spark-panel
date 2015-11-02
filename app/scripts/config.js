4bfd7785f08a239e22205cd861227bf12cf428e9
Guillaume Nominé <gnomine@ibsciss.com>
2015-11-02 16:47:43 +0100

    UPDATE

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,7 +2,7 @@
 * Created by bshen on 08/09/15.
 */
 var config = {
-    appUrl: 'http://localhost:1337',
-    //appUrl: 'https://backend-spark.scalingo.io',
+    //appUrl: 'http://localhost:1337',
+    appUrl: 'https://backend-spark.scalingo.io',
     assetsUrl: 'http://spark-panel.s3-website-us-east-1.amazonaws.com'
 };
